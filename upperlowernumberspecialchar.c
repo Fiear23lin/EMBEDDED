ce8ba41a5ea3d27adbba60b18acf4270f7f9ddf4
Fiearlin mercy <167228607+Fiear23lin@users.noreply.github.com>
2024-04-17 15:21:46 +0530

    switch case program
    hy

---
+++ upperlowernumberspecialchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a='-';
+    //printf("enter the character\n");
+    //scanf("%c",&a);
+    switch(a)
+    {
+        case 'A' ... 'Z':
+        printf("its uppercase\n");
+        break;
+         case 'a' ... 'z':
+        printf("its lowercase\n");
+        break;
+         case '0' ... '9':
+        printf("its a number\n");
+        break;
+        default:
+        printf("its  a special character\n");
+    }
+}
